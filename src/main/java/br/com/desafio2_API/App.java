075e69c764371c882e1a950cadcf281adf2cd099
Raquel de Cássia <rcassia_scarvalho@hotmail.com>
2020-11-02 20:41:01 -0300

    Initial commit

---
+++ src/main/java/br/com/desafio2_API/App.java
@@ -0,0 +1,13 @@
+package br.com.desafio2_API;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/br/com/desafio2_API/ApiTests.java
@@ -0,0 +1,35 @@
+package br.com.desafio2_API;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.Test;
+import io.restassured.http.ContentType;
+
+public class ApiTests {
+	
+	private static  String ID_DO_FILME = "tt1285016";
+	private static  String API_KEY = "52ec71bf";
+	
+	
+	@Test
+	public void validarNomeAnoIdioma() {
+		
+		String endpoint = "http://www.omdbapi.com/";
+			
+		given()
+		.relaxedHTTPSValidation()
+		.param("i", ID_DO_FILME) 
+		.param("apikey", API_KEY) 
+		.when()
+		.get(endpoint)
+		.then()
+		.statusCode(200) 
+		.contentType(ContentType.JSON) 
+		.body("Title", equalTo("The Social Network"))
+		.body("Year", equalTo("2010")) 
+		.body("Language", equalTo("English, French"));
+	}
+	
+
+	
+}
